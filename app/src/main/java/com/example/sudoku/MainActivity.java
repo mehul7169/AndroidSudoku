e214ad35f324ba9834c23cd9afe21826aea4c1b9
Mehul <mehul7169@gmail.com>
2023-04-06 14:19:34 -0400

    Updating mainActivity.java

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -1,23 +1,44 @@
-package com.example.sudoku;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    private Button playButton;
+    private RadioGroup difficultyRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playButton = findViewById(R.id.playButton);
-        setPlayButtonOnClickListener(playButton);
+
+        difficultyRadioGroup = findViewById(R.id.radio_group_difficulty);
+        Button startButton = findViewById(R.id.button_start);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int difficulty = getDifficulty();
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                intent.putExtra("difficulty", difficulty);
+                startActivity(intent);
+            }
+        });
     }
 
-    private void setPlayButtonOnClickListener(Button playButton) {
-
+    private int getDifficulty() {
+        int checkedRadioButtonId = difficultyRadioGroup.getCheckedRadioButtonId();
+        switch (checkedRadioButtonId) {
+            case R.id.radio_easy:
+                return 1;
+            case R.id.radio_medium:
+                return 2;
+            case R.id.radio_hard:
+                return 3;
+            default:
+                return 1;
+        }
     }
 }
